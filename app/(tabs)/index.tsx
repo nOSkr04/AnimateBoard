6f99caba1b66ae3cf7859384bc3a8cdda98ee044
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2024-05-28 16:27:14 +0800

    sync

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,85 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { FlatList, StyleSheet, View, ViewToken } from "react-native";
+import React, { useCallback } from "react";
+import { ThemedView } from "@/components/ThemedView";
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import { OnboardingData, data } from "./data";
+import { BoardCard } from "./board-card";
+import { Pagination } from "./pagination";
+import { BoardButton } from "./board-button";
 
 export default function HomeScreen() {
+  const flatlistRef = useAnimatedRef<FlatList<OnboardingData>>();
+  const x = useSharedValue(0);
+  const flatlistIndex = useSharedValue(0);
+  const renderBoard = useCallback(
+    ({ item, index }: { item: OnboardingData; index: number }) => {
+      return <BoardCard item={item} index={index} x={x} />;
+    },
+    []
+  );
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x;
+    },
+  });
+
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      if (viewableItems[0].index !== null) {
+        flatlistIndex.value = viewableItems[0].index;
+      }
+    },
+    []
+  );
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <ThemedView style={styles.container}>
+      <Animated.FlatList
+        ref={flatlistRef}
+        data={data}
+        onScroll={onScroll}
+        renderItem={renderBoard}
+        keyExtractor={(item) => item.id.toString()}
+        scrollEventThrottle={16}
+        horizontal
+        onViewableItemsChanged={onViewableItemsChanged}
+        bounces={false}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        viewabilityConfig={{
+          minimumViewTime: 300,
+          viewAreaCoveragePercentThreshold: 10,
+        }}
+      />
+      <View style={styles.bottomContainer}>
+        <Pagination data={data} x={x} />
+        <BoardButton
+          flatlistRef={flatlistRef}
+          flatlistIndex={flatlistIndex}
+          dataLength={data.length}
+          x={x}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
+  bottomContainer: {
+    position: "absolute",
+    bottom: 20,
     left: 0,
-    position: 'absolute',
+    right: 0,
+    marginHorizontal: 30,
+    padding: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 });
